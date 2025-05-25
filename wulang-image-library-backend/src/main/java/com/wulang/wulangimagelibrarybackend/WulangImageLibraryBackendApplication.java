40529d2257d3a566db28b318a2674fc77ccd30d1
MaoWuLang <1257635375@qq.com>
2025-05-26 02:03:00 +0800

    backend initialization

---
+++ wulang-image-library-backend/src/main/java/com/wulang/wulangimagelibrarybackend/WulangImageLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wulang.wulangimagelibrarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WulangImageLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WulangImageLibraryBackendApplication.class, args);
+    }
+
+}
---
+++ wulang-image-library-backend/src/test/java/com/wulang/wulangimagelibrarybackend/WulangImageLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wulang.wulangimagelibrarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WulangImageLibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
